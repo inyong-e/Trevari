be910808051b762f991131928d796630512b89f7
Inyong Jung <jiy8319@gmail.com>
2019-07-16 17:12:59 +0900

    adjusting Card View

--- src/components/Meeting/MeetingCard.tsx
+++ src/components/Meeting/MeetingCard.tsx
@@ -7,9 +7,11 @@ interface Props{
 }
 
 export const MeetingCard:React.SFC<Props> = ({CardDesc}) => (
-  <Card style={{border:0}}>
-    <div style={{ height:'180px', marginBottom:'5px'}}>
+  <div style={{margin:'10px'}}>
+    <div style={{ height:'200px', marginBottom:'5px', position:'relative'}}>
+    <div style={{backgroundColor:'#ff8906',width:'100%',position:'absolute', opacity:0.85, color:'white',textAlign:'center',fontSize:'1rem'}}>그룹장 트레바리</div>
       <img src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png' style={{width:'100%',height:'100%'}}/>
+      
     </div>
 
     <div>
@@ -18,5 +20,5 @@ export const MeetingCard:React.SFC<Props> = ({CardDesc}) => (
       <div>강남 아지트<br />2019년 07월 17일 수요일 오후 07시 40분</div>
     </div>
     
-  </Card>
+  </div>
 )
