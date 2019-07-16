50ad4ce0ec9ff8e6cb693928a05ab86dfcf58322
Inyong Jung <jiy8319@gmail.com>
2019-07-16 22:11:16 +0900

    completed realtime Search

--- src/components/Meeting/MeetingCardField.tsx
+++ src/components/Meeting/MeetingCardField.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
-import {Cards} from '../../store/MeetingReducer';
+import {CardDesc} from '../../store/common';
 import {MeetingCard} from './MeetingCard';
 
-export const MeetingCardField:React.SFC<Cards> = ({Cards}) => {
+interface Props{
+  Keyword:string,
+  Cards: CardDesc[]
+}
 
-  const CardList = Cards.map(card => (
-    <div style={{display:'inline-block', width:'33.3%'}}>
+export const MeetingCardField:React.SFC<Props> = ({Cards, Keyword}) => {
+
+  const CardList = Cards.filter(card=> card.hostGroup.includes(Keyword)).map(card => (
+    <div style={{display:'inline-block', verticalAlign:'top' ,width:'33.3%'}}>
       <MeetingCard CardDesc={card}  />
     </div>
   ));
 
   return(
     <div>
-      {CardList}
+      {CardList.length ? CardList : (<EmptyView />)}
     </div>
   );
 }
+
+const EmptyView = () => (
+  <div style={{paddingTop:'10%', paddingBottom:'10%',textAlign:'center', fontSize:'1.2rem'}}>
+    검색 결과가 없습니다...😭
+  </div>
+)
