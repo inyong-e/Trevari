5cc83fed4fc98242d39b800174e399f025b4af16
Inyong Jung <jiy8319@gmail.com>
2019-07-18 22:47:51 +0900

    adjusting FAQ list key error

--- src/components/FAQ/QuestionField.tsx
+++ src/components/FAQ/QuestionField.tsx
@@ -10,7 +10,7 @@ const QuestionField:React.SFC<Props> = ({QueData, selectCate}) => (
   <div style={{margin:'0 5%'}}>
     {QueData.filter(Que => Que.Category === selectCate)
     .map(Que=>(
-      <Question Que={Que}/>
+      <Question key={Que.Question} Que={Que}/>
     ))}
   </div>
 )
